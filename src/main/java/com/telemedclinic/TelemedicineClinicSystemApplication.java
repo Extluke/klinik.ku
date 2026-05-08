dbb8e45129d5721fece48a77763b4cdd2433e29b
renwxyz <rendirendi11122@gmail.com>
2026-05-08 12:56:52 +0700

    Initial Spring Boot project setup

---
+++ src/main/java/com/telemedclinic/TelemedicineClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.telemedclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedicineClinicSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telemedclinic/TelemedicineClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemedclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicineClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
